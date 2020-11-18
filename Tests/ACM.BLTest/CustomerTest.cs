cdf0a57a08976d361b53540a8cbc9582d13b4f57
Aniket Deotale <43133617+aniketdeotale@users.noreply.github.com>
2020-11-18 23:43:48 +0530

    Validating the First and last name, testing.

--- Tests/ACM.BLTest/CustomerTest.cs
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -16,11 +16,59 @@ namespace ACM.BLTest
                 LastName = "Deotale"
             };
 
-            string expected = "Deotale , Aniket";
+            string expected = "Deotale, Aniket";
 
             string actual = customer.FullName;
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            Customer customer = new Customer
+            {
+               
+                LastName = "Deotale"
+            };
+
+            string expected = "Deotale";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            Customer customer = new Customer
+            {
+
+                FirstName = "Aniket"
+            };
+
+            string expected = "Aniket";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void StaticTest()
+        {
+            var c1 = new Customer();
+            c1.FirstName = "Aniket";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c1.FirstName = "Anuj";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c1.FirstName = "Ashwin";
+            Customer.InstanceCount += 1;
+
+            Assert.AreEqual(3, Customer.InstanceCount);
         }
     }
 }
